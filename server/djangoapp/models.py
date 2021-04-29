e91c3695cc91cee3c841b22ba887a88999c5a78d
james <jamesjcampbell1@gmail.com>
2021-04-29 20:38:57 +0000

    week 3

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -2,24 +2,35 @@ from django.db import models
 from django.utils.timezone import now
 
 
-# Create your models here.
+class CarMake(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(null=False, max_length=50)
+    description = models.CharField(null=False, max_length=200)
+    
+    def __str__(self):
+        return "Name: " + self.name + ", " \
+            "Description: " + self.description
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=80)
+    dealer_id = models.IntegerField(null=False)
+    year = models.DateField(null=False, default=now)
 
+    TYPE_OPTIONS = [
+        ('sedan', 'Sedan'),
+        ('suv', 'SUV'),
+        ('wagon', 'WAGON')
+    ]
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+    car_type = models.CharField(null=False, max_length=20, choices=TYPE_OPTIONS)
 
+    def __str__(self):
+        return "Name: " + self.name + " Make: " \
+            + self.car_make.name + " Type: " \
+            + self.car_type + " Year: " \
+            + str(self.year) + " Dealer ID: " + str(self.dealer_id)
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
